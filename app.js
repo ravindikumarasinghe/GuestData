dff0241664f85c82b2d1b90c25eb8b506b15278b
ravindikumarasinghe <ravindikumarasinghe2@gmail.com>
2023-03-13 15:06:36 +0530

    create functions and export from module

---
+++ app.js
@@ -0,0 +1,9 @@
+const db =  require("./guestdb.js");
+
+// get input data from console
+
+// console.log("app.js");
+
+console.log(db.add());
+console.log(db.view());
+
---
+++ guestdb.js
@@ -0,0 +1,22 @@
+
+
+// save data in a file
+
+// console.log("db.js");
+
+// const name = "Ravindi";
+
+//create
+const add =()=>{
+    return "adding";
+}
+
+// view function
+const view =()=> "view";
+
+
+
+module.exports = {
+    add,
+    view
+};
